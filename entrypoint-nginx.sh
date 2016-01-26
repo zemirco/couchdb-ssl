e84ba06cd59a74d35ee34b49c16c54f84cc1e017
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-01-26 12:56:38 +0100

    add chain with key, certificate and dhparams

--- entrypoint-nginx.sh
+++ entrypoint-nginx.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 set -e;
 
-# chown _nginx: /usr/local/etc/stud/stud.pem
-# chown _nginx: /var/run/stud
-# chown -R _nginx: /usr/local/var/run/stud /usr/local/etc/stud
-# chmod 0770 /usr/local/var/run/stud
-#
-# chmod 664 /usr/local/etc/stud/*.conf
-# chmod 600 /usr/local/etc/stud/stud.pem
-
-mkdir /var/run/nginx
-# useradd -d /var/run/nginx _nginx
-# gosu _nginx nginx
-# gosu _nginx nginx
-
+# start nginx in the background
 nginx -g 'daemon on;'
 
+# start couchdb
 exec /docker-entrypoint.sh couchdb
